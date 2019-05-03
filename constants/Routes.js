b8c8be847112073ccd2e56c18fc12b76f241fbd4
Matias Gonzalez Casas <Mgonzalezglo@globalmvd.com.uy>
2019-05-03 17:56:06 -0300

    countries list

---
+++ constants/Routes.js
@@ -0,0 +1,4 @@
+export default {
+    url: "https://apifootball.com/api/",
+    apiKey: 'e108efc813e390acfae414784da4bffad5066f1544bf74b5afb223768d5947df'
+};
--- screens/LinksScreen.js
+++ screens/LinksScreen.js
@@ -1,27 +1,67 @@
 import React from 'react';
-import { ScrollView, StyleSheet } from 'react-native';
-import { ExpoLinksView } from '@expo/samples';
+import { ScrollView, StyleSheet, FlatList } from 'react-native';
+import { SearchBar, ListItem } from 'react-native-elements';
+import routes from '../constants/Routes';
 
 export default class LinksScreen extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { searchText: "", countries: [] }
+  }
+
+  componentWillMount() {
+    this.getData();
+  }
+
+  getData = () => {
+    fetch(`${routes.url}?APIkey=${routes.apiKey}&action=get_countries`)
+      .then(response => response.json())
+      .then(parsedJson => this.setState({ countries: parsedJson }))
+      .catch(error => { })
+  }
+
   static navigationOptions = {
-    title: 'Links',
+    title: 'Select a country',
   };
 
   render() {
     return (
       <ScrollView style={styles.container}>
-        {/* Go ahead and delete ExpoLinksView and replace it with your
-           * content, we just wanted to provide you with some helpful links */}
-        <ExpoLinksView />
+        <SearchBar
+          placeholder="Type here..."
+          onChangeText={this.onChangeTextSearchBar}
+          value={this.state.searchText}
+        />
+        <FlatList
+          keyExtractor={item => item.country_id}
+          data={this.state.countries}
+          renderItem={this.renderItem}
+        />
       </ScrollView>
     );
+  }
+
+  onChangeTextSearchBar = () => {
+    const filtered = this.state.countries.filter(c => c.country_name.includes(this.state.searchText));
+    console.log(this.state.searchText)
+    this.setState({ countries: filtered });
+  }
+
+  renderItem = ({ item }) => {
+    return (
+      <ListItem
+        title={item.country_name}
+        chevron={true}
+        bottomDivider
+        onPress={this.onListItemPress}
+      />
+    )
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: 15,
     backgroundColor: '#fff',
   },
 });
